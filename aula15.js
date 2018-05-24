f152eca62ba5bc01d80040ec5a0696810f14e6b7
GABRIELROLAND <32078927+GABRIELROLAND@users.noreply.github.com>
2018-05-23 22:02:01 -0300

    Add files via upload

---
+++ aula15.js
@@ -0,0 +1,27 @@
+function changeText (id) {
+	id.innerHTML="Olá!!!boa noite!!!";
+	}
+	function displayDate(){
+document.getElementById("demo").innerHTML=Date();
+	}
+function mOver (obj)
+{
+	obj.innerHTML="oiiiiiiiiii!!!!!";
+}
+function mOut(obj){
+obj.innerHTML="Tchau!!!";
+}
+function mDown(obj)
+{
+	obj.style.backgroundColor="#1EC5E5";
+	obj.innerHTML="Olá!!!";
+}
+function mUp(obj)
+{
+	obj.style.backgroundColor="#CECECE";
+	obj.innerHTML="Ate mais!!!!";
+}
+function myFunction(){
+document.getElementById("demo1").innerHTML="Comando novo";
+
+}
